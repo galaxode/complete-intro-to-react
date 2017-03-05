2b760623d7ac655b790628e2f6876d3ae398f073
Andrew Cookro <andrew.cookro@gmail.com>
2017-03-05 14:35:42 -0500

    Add ajax call using axios to pull imdb rating from omdb in Details

--- js/Details.js
+++ js/Details.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import Header from './Header'
 const { shape, string } = React.PropTypes
 
@@ -9,17 +10,40 @@ const Details = React.createClass({
       year: string.isRequired,
       poster: string.isRequired,
       trailer: string.isRequired,
-      descriptions: string.isRequired
+      description: string.isRequired,
+      imdbID: string.isRequired
+    })
+  },
+  getInitialState () {
+    return {
+      omdbData: {}
+    }
+  },
+  componentDidMount () {
+    axios.get(`http://www.omdbapi.com/?i=${this.props.show.imdbID}`)
+    .then((response) => {
+      console.log('response', response)
+      this.setState({omdbData: response.data})
+    })
+    .catch((error) => {
+      console.error('axios error', error)
     })
   },
   render () {
     const { title, description, year, poster, trailer } = this.props.show
+    let rating
+    if (this.state.omdbData.imdbRating) {
+      rating = <h3>{this.state.omdbData.imdbRating}</h3>
+    } else {
+      rating = <img src='../public/img/loading.png' alt='loading indicator' />
+    }
     return (
       <div className='details'>
         <Header />
         <section>
           <h1>{title}</h1>
           <h2>({year})</h2>
+          {rating}
           <img src={`/public/img/posters/${poster}`} />
           <p>{description}</p>
         </section>
