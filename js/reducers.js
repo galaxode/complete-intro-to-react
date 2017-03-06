cc08746ecba8ea376292c6c22240961341e6c79d
Andrew Cookro <andrew.cookro@gmail.com>
2017-03-06 17:31:24 -0500

    Add the setSearchTerm reducer

--- js/reducers.js
+++ js/reducers.js
@@ -1,9 +1,18 @@
+import { SET_SEARCH_TERM } from './actions'
+
 const DEFAULT_STATE = {
   searchTerm: ''
 }
 
+const setSearchTerm = (state, action) => {
+  const newState = {}
+  Object.assign(newState, state, {searchTerm: action.searchTerm})
+}
+
 const rootReducer = (state = DEFAULT_STATE, action) => {
   switch (action.type) {
+    case SET_SEARCH_TERM:
+      return setSearchTerm(state, action)
     default:
       return state
   }
