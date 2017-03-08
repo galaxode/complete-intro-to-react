b78ddc476b7f09096d0b53b04fb19e8d65c12ba0
Andrew Cookro <andrew.cookro@gmail.com>
2017-03-08 18:50:53 -0500

    Add thunk middleware to redux store

--- js/store.js
+++ js/store.js
@@ -1,9 +1,12 @@
-import { createStore, compose } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
 const store = createStore(
   rootReducer,
-  compose(typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : (f) => f)
+  compose(
+    applyMiddleware(thunk),
+    typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : (f) => f)
 )
 
 export default store
