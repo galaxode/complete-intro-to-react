c1794291cd7eab553223225019104f73d91eeba9
Andrew Cookro <andrew.cookro@gmail.com>
2017-06-14 17:25:28 -0400

    Add code splitting with webpack and async routing

--- js/App.js
+++ js/App.js
@@ -2,26 +2,43 @@ import React from 'react'
 import { Match } from 'react-router'
 import { Provider } from 'react-redux'
 import store from './store'
-import Landing from './Landing'
-import Search from './Search'
-import Details from './Details'
+import AsyncRoute from './AsyncRoute'
 import preload from '../public/data.json'
+if (global) {
+  global.System = { import () {} }
+}
 
 const App = () => {
   return (
     <Provider store={store}>
       <div className='app'>
-        <Match exactly pattern='/' component={Landing} />
+        <Match
+          exactly
+          pattern='/'
+          component={(props) => <AsyncRoute
+            props={props}
+            loadingPromise={System.import('./Landing')} />
+            // './Landing' must be string not a variable holding a string
+            // Because webpack uses static analysis so it cannot be dynamic
+          }
+        />
         <Match
           pattern='/search'
-          component={(props) => <Search shows={preload.shows} {...props} />}
+          component={(props) => <AsyncRoute
+            props={Object.assign({shows: preload.shows}, props)}
+            loadingPromise={System.import('./Search')} />
+            // './Search' must be string not a variable holding a string
+          }
 
         />
         <Match
           pattern='/details/:id'
           component={(props) => {
             const shows = preload.shows.filter((show) => props.params.id === show.imdbID)
-            return <Details show={shows[0]} {...props} />
+            return <AsyncRoute
+              props={Object.assign({show: shows[0]}, props)}
+              loadingPromise={System.import('./Details')} />
+              // './Details' must be string not a variable holding a string
           }}
         />
       </div>
